1aed31696beec9701272d18c110d4391d498723e
Jake <jdragonrider@hotmail.com>
2020-08-29 17:08:32 -0600

    Room switch script

---
+++ Assets/Scripts/RoomSwitch.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomSwitch : MonoBehaviour
+{
+    [SerializeField] private Transform room1Center;
+    [SerializeField] private Transform room2Center;
+    private bool atRoomOne = true;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("2"))
+        {
+            if (atRoomOne)
+            {
+                transform.position = room2Center.position;
+            }
+            else
+            {
+                transform.position = room1Center.position;
+            }
+
+            atRoomOne = !atRoomOne;
+        }
+    }
+}
