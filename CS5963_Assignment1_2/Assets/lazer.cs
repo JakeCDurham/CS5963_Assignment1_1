370a19fc728841d6e6b505b19111d39f0d8cdcf0
Jake <jdragonrider@hotmail.com>
2020-09-14 17:41:11 -0600

    Added LAZERS! working on grabbing them.
    Yes, I know it is spelled 'lasers.'

---
+++ CS5963_Assignment1_2/Assets/lazer.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class lazer : MonoBehaviour
+{
+    private bool inZone = false;
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            inZone = true;
+        }
+    }
+
+    private void OnCollisionExit(Collision other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            inZone = false;
+        }
+    }
+
+    private void Update()
+    {
+        if (OVRInput.Get(OVRInput.Axis1D.PrimaryHandTrigger) > 0)
+        {
+            Debug.Log("Grabbed.");
+        }
+    }
+}
---
+++ CS5963_Assignment1_2/Assets/lazerController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Net.Http.Headers;
+using UnityEngine;
+
+public class lazerController : MonoBehaviour
+{
+    [SerializeField] private List<GameObject> lazers;
+    [SerializeField] private float switchTime = 3f;
+    private int currentIndex;
+    private int score;
+    private float timeLeft;
+    private bool triggerSwitch = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        timeLeft = switchTime;
+        foreach (GameObject G in lazers)
+        {
+            G.SetActive(false);
+        }
+        lazers[0].SetActive(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeLeft -= Time.deltaTime;
+
+        if (timeLeft <= 0 || triggerSwitch)
+        {
+            timeLeft = switchTime;
+            triggerSwitch = false;
+            int temp = currentIndex;
+            while (temp == currentIndex)
+            {
+                temp = Random.Range(0, lazers.Count);
+            }
+
+            lazers[currentIndex].SetActive(false);
+            lazers[temp].SetActive(true);
+            currentIndex = temp;
+        }
+    }
+
+    public void SwitchLights()
+    {
+        triggerSwitch = true;
+    }
+}
